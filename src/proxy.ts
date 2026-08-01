fc0269a8fec8fac5797edbf59e74bc8993e4e376
KellyM12 <kmasaki2@hawaii.edu>
2026-07-31 15:19:08 -1000

    landing page mockup finished in 27 minutes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,9 @@
-//import { NextRequest, NextResponse } from "next/server";
+import { NextResponse} from "next/server";
 
-// 1. You MUST explicitly name the function 'proxy' and export it 👈 THE FIX
-export async function proxy(request: NextRequest) {
-  
-  // Your template's proxy or authentication logic goes here.
-  // For now, this safely allows requests to proceed through the application:
+export async function proxy() {
   return NextResponse.next();
 }
 
-// 2. Keep your matching paths rule intact at the bottom
 export const config = {
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
